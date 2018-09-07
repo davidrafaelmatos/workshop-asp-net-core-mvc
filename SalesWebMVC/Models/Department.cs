2de2a0576be0baf049b1e8176fec68da42417499
davidrafaelmatos <davidmatos@ipvc.pt>
2018-09-07 09:34:13 +0100

    Others entities and Migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -10,6 +10,27 @@ namespace SalesWebMVC.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Department()
+        {
+        }
+
+        public void AddSeller(Seller Seller)
+        {
+            Sellers.Add(Seller);
+        }
+
+        public double TotalSales(DateTime Initial, DateTime Final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(Initial, Final));
+        }
 
     }
 }
