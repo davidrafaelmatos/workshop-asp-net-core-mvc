3420c4d418982666070fed5b1fc018c260f8826a
davidrafaelmatos <davidmatos@ipvc.pt>
2018-09-07 16:20:30 +0100

    Async Service

--- SalesWebMVC/Services/DepartmentService.cs
+++ SalesWebMVC/Services/DepartmentService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMVC.Services
 {
@@ -16,9 +17,9 @@ namespace SalesWebMVC.Services
             _context = context;
         }
 
-        public List<Department> findAll()
+        public async Task<List<Department>> findAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
